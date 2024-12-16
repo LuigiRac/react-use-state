3abcee14196f3a88de4d89f1f099510ae8999ffb
LuigiRac <lui.rac06@gmail.com>
2024-12-16 14:43:02 +0100

    react_button

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CssContent from "./Components/CssContent.jsx";
 import JsContent from "./Components/JsContent.jsx";
 import NodeContent from "./Components/NodeContent.jsx";
 import ExpressContent from "./Components/ExpressContent.jsx";
+import ReactContent from "./Components/ReactContent.jsx";
 import './App.css'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <JsContent />
         <NodeContent />
         <ExpressContent />
+        <ReactContent />
       </div>
 
     </>
---
+++ src/Components/ReactContent.jsx
@@ -0,0 +1,12 @@
+function ReactContent() {
+
+
+    return (
+        <button class="btn btn-primary" type="submit">ReactJS</button>
+
+    )
+};
+
+
+
+export default ReactContent;
