320977f4eb12d76e55403d8d0ca5862b9db95326
LuigiRac <lui.rac06@gmail.com>
2024-12-17 20:27:08 +0100

    useState/ end_exercise

--- src/Components/ButtonDisplay.jsx
+++ src/Components/ButtonDisplay.jsx
@@ -1,20 +1,26 @@
+import { useState } from "react";
 import languages from "../data/languages"
 
 function ButtonDisplay() {
     // console.log(languages);
-
     const languagesList = [...languages];
+    const [activeLanguages, setActiveLanguages] = useState(languagesList[0]);
+
+
     return (
         <>
             <section className="container">
                 <ul className="d-flex gap-2">
                     {
-                        languagesList.map((languages) => <button className="btn btn-primary" type="submit" key={languages.id}>{languages.title}</button>
+                        languagesList.map((languages) =>
+                            <button className="btn btn-primary" type="submit" onClick={() => setActiveLanguages(languages)} key={languages.id}>{languages.title}
+                            </button>
                         )
                     }
                 </ul>
-                <div className="box">
-
+                <div className="box d-flex flex-wrap p-3">
+                    <h4>{activeLanguages.title}</h4>
+                    {activeLanguages.description}
                 </div>
             </section>
         </>
